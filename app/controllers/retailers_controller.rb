9ac8f316c23bbec47d34459c7e99ac459f5b8451
saikb <saikb@ubuntu.ubuntu-domain>
2020-02-04 12:18:05 +0530

    theme integration

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -3,14 +3,25 @@ class RetailersController < ApplicationController
   end
 
   def new
+    @retailer = Retailer.new
   end
 
-  def index
+  def index 
+    @retailers = Retailer.all
   end
 
   def update
   end
 
   def create
+    @retailer = Retailer.new(retailer_params)
+    @retailer.save
+    redirect_to retailer_path
   end
+
+  private
+  def retailer_params
+    params.require(:retailer).permit(:code, :shop_name, :retailer_name, :mobile_number1, mobile_number2, :address)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'retailers/edit'
-  get 'retailers/new'
-  get 'retailers/index'
+  get '/retailers/new', :to => "retailers#new", :as => "new_retailer"
+  get '/retailers/index', :to => "retailers#index", :as => "retailer"
   get 'retailers/update'
-  get 'retailers/create'
+  get '/retailers/create', :to => "retailers#create", :as => "create_retailer"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
